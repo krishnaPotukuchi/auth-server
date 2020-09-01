9e43b54d30f87d956ac6f7fd6b98cf9dbc2af74a
Krishna <pkkrishnaa@yahoo.com>
2020-09-01 15:44:33 +0530

    Auth server impl

---
+++ src/main/java/com/krishna/auth/authserver/conf/UserConfiguration.java
@@ -0,0 +1,20 @@
+package com.krishna.auth.authserver.conf;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class UserConfiguration extends GlobalAuthenticationConfigurerAdapter {
+	
+	PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	
+	@Override
+	public void init(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("krish").password(encoder.encode("krish123")).roles("ADMIN","USER").authorities("CAN_READ", "CAN_WRITE","CAN_DELETE");
+		
+	}
+
+}
